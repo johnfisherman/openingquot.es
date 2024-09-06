b7659a6ea346d0a17f3bb90ae5faaf27a2fc0b02
Fred Rocha <john.fisherman@gmail.com>
2024-09-06 17:10:11 +0100

    Show single book. Link to single book pages from main page.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "books#index"
 
-  get "books", to: "books#index"
+  resources :books
+
   # get "authors", to: "authors#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
