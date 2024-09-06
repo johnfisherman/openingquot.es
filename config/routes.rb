b2ad98ee4db0d9488bb4f77a4308c61011fa124f
Fred Rocha <john.fisherman@gmail.com>
2024-09-06 11:52:22 +0100

    First actual commit with code. A lot of scaffolding stuff. Introduce resource Books.

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "books#index"
+
+  get "books", to: "books#index"
+  # get "authors", to: "authors#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
