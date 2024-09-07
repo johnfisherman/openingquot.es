fb15d961610d220f483229e116202be1964ba234
Fred Rocha <john.fisherman@gmail.com>
2024-09-07 23:00:52 +0100

    List books for a specific author.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,9 @@ class AuthorsController < ApplicationController
     def index
         @authors = Author.all
     end
+
+    def show
+        @author = Author.find(params[:id])
+        @author_books = Book.where(author_id: @author.id)
+    end
 end
