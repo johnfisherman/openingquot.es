a6f28f5e0d728c95239d17fa75fdc74e48196680
Fred Rocha <john.fisherman@gmail.com>
2024-09-09 19:20:06 +0100

    Force CSS compilation in production.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 bundle install
 bundle exec rake assets:precompile
+bundle exec rake dartsass:build
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
