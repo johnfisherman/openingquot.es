d0b2d8c7a52667a78962d316a987d1a54c269632
Fred Rocha <john.fisherman@gmail.com>
2024-09-09 15:35:19 +0100

    Lay down basic seed content for app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Populate Authors
+["Paul Auster", "Salman Rushdie", "Fred Rocha", "Ernest Hemingway", "Gertrude Stein", "Hunter S. Thompson", "Samuel Johnson", "Ray Bradbury", "Juan Ramón Jiménez"].each do |author_name|
+    Author.find_or_create_by!(name: author_name)
+end
+
+# Populate Books
+Book.new(title: "Victory City", year: 2023, author_id: 2,).save
+Book.new(title: "Ilha da Contera", year: 2022, author_id: 3).save
+Book.new(title: "The Satanic Verses", year: 1988, author_id: 2).save
+Book.new(title: "The Sun Also Rises", year: 1988, author_id: 4).save
+Book.new(title: "Fear and Loathing in Las Vegas", year: 1971, author_id: 6).save
+Book.new(title: "Fahrenheit 451", year: 1953, author_id: 8).save
+
+# Populate Quotes
+Quote.new(body: "You are all a lost generation.", book_id: 4, author_id: 5).save
+Quote.new(body: "He who makes a beast of himself gets rid of the pain of being a man.", book_id: 5, author_id: 7).save
+Quote.new(body: "If they give you ruled paper, write the other way.", book_id: 6, author_id: 9).save
