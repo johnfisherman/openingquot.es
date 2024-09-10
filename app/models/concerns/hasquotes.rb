6f9df653d71d5a288e2bb7bb6ccb5691b4eaaa6c
User <user@Users-MacBook-Pro.local>
2024-09-10 20:06:47 +0100

    Simplify some of the quote presence logic

--- app/models/concerns/hasquotes.rb
+++ app/models/concerns/hasquotes.rb
@@ -4,10 +4,6 @@ module Hasquotes
   #Does this book open with a quote?
   def has_quote?
     @quote = Quote.find_by(book_id: id)
-    if @quote.nil? || @quote.body.blank?
-      return false
-    else 
-      return true
-    end
+    @quote&.body.present?
   end
 end
