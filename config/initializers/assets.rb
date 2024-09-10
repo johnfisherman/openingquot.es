cd38f19d5eed411e940cd6129a4495bae9ff6357
Fred Rocha <john.fisherman@gmail.com>
2024-09-10 08:20:40 +0100

    Tell Rails not to compile SCSS using Sprocket.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 # Rails.application.config.assets.precompile += %w[ builds/application.css ]
-Rails.application.config.assets.precompile += %w[ application.css ]
+Rails.application.config.assets.precompile += %w[ application.scss ]
 
