8045a0ae138306f4af67876fd69baa25d6d967f0
Fred Rocha <john.fisherman@gmail.com>
2024-09-10 09:55:07 +0100

    Disable DB drop protection in production. Living dangerously.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,8 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake dartsass:build
 bundle exec rake assets:clean
-bundle exec rake  db:drop db:create db:migrate db:seed
+bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:create db:migrate db:seed
 bundle exec rake db:seed
 
 # sass app/assets/style.scss:app/assets/stylesheets/style.css --style compressed
