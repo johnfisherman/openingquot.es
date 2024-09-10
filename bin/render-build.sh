f16023942a178fe09bca810af7a0acf7ad30fb8c
Fred Rocha <john.fisherman@gmail.com>
2024-09-10 10:14:58 +0100

    [FIX] Remove double seeding of quotes, in production.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,5 @@ bundle exec rake dartsass:build
 bundle exec rake assets:clean
 bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:create db:migrate db:seed
-bundle exec rake db:seed
 
 # sass app/assets/style.scss:app/assets/stylesheets/style.css --style compressed
