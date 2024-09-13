2323363a19c23b13e879e877a83869cc216955bd
Fred Rocha <john.fisherman@gmail.com>
2024-09-13 19:28:05 +0100

    Author page times she was quoted and times she quoted.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,5 +6,8 @@ class AuthorsController < ApplicationController
     def show
         @author = Author.find(params[:id])
         @author_books = Book.where(author_id: @author.id)
+        @quotes = Quote.all
+        @quoted_quotes = @quotes.where(author_id: @author.id)
+
     end
 end
