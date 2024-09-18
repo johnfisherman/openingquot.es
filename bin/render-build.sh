0fb8826f5ab7385bae6b18d8ca0dad32d6cb67e2
Fred Rocha <john.fisherman@gmail.com>
2024-09-18 19:57:59 +0100

    Keep DB intact on deployment.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake dartsass:build
 bundle exec rake assets:clean
-bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rake db:create db:migrate db:seed
+# bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rake db:create db:migrate db:seed
 
 # sass app/assets/style.scss:app/assets/stylesheets/style.css --style compressed
