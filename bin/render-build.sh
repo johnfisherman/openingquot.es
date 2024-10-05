7b3921ec63c518ac83ad8cdf3d83b457291679ea
Fred Rocha <john.fisherman@gmail.com>
2024-10-05 20:32:57 +0100

    Add DB migrate to deploy scripts.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,7 @@ bundle exec rake dartsass:build
 bundle exec rake assets:clean
 # generate sitemap
 bundle exec rake sitemap:refresh
+bundle exec rake db:migrate 
 
 # bundle exec rake RAILS_ENV=production db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # bundle exec rake db:create db:migrate db:seed
